47e968e85d698d641d86435c1caebd57ae28056a
Theodore Nguyen <theodorenguyen5@gmail.com>
2021-01-06 15:32:32 -0800

    Creating cards

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,6 +1,6 @@
 import '../../App.css';
-import HeroSection from '../HeroSection.js'
-import Cards from '../Cards.js';
+import HeroSection from '../HeroSection'
+import Cards from '../Cards';
 
 function Home() {
   return (
