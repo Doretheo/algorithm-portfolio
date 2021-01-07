4f045b6b280bdfb529008ef9596c327c2c5ee68a
Theodore Nguyen <theodorenguyen5@gmail.com>
2021-01-06 16:52:20 -0800

    Creating the footer

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import Navbar from './components/Navbar.js'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
 import Home from './components/pages/Home'
+import Services from './components/pages/Home'
+import Products from './components/pages/Home'
+import SignUp from './components/pages/Home'
 
 function App() {
   return (
@@ -11,6 +14,9 @@ function App() {
         <Navbar />
         <Switch>
           <Route path='/' exact component={Home}/>
+          <Route path='/' exact component={Services}/>
+          <Route path='/' exact component={Products}/>
+          <Route path='/' exact component={SignUp}/>
         </Switch>
       </Router>
     </>
